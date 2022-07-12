ea26b4a1d8d792a24d18332b37b2982970fbb89b
Abhishek Sagar <abhisheksagar1403@gmail.com>
2022-07-12 14:20:54 +0530

    Modified:Initialised Variables

--- src/com/bridgelabs/GamblerProblem/GamblingProblem.java
+++ src/com/bridgelabs/GamblerProblem/GamblingProblem.java
@@ -1,6 +1,10 @@
 package com.bridgelabs.GamblerProblem;
 
 public class GamblingProblem {
+
+    static int DAILY_STAKE = 100;
+    static int PER_DAY_BET = 1;
+    
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
     }
