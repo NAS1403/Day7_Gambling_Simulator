bfe76547a7fbf7e7c45cad2184094eb48a48d260
Abhishek Sagar <abhisheksagar1403@gmail.com>
2022-07-12 15:41:02 +0530

    Modified: day lost and day won

--- src/com/bridgelabs/GamblerProblem/GamblingProblem.java
+++ src/com/bridgelabs/GamblerProblem/GamblingProblem.java
@@ -1,14 +1,16 @@
 package com.bridgelabs.GamblerProblem;
 
-import java.util.Date;
-
 public class GamblingProblem {
 
     static int DAILY_STAKE = 100;
     static int PER_DAY_BET = 1;
     static int win = 1;
+    static int daysWon=0;
+    static int daysLost=0;
+    static int totalAmountWonFor20Day=0;
+    static int totalAmountLostFor20Day=0;
 
-    
+
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
         for (int day =1; day<=20;day++) {
@@ -17,13 +19,28 @@ public class GamblingProblem {
                 int result = (int) (Math.random() * 10) % 2;
                 if (result == win) {
                     DAILY_STAKE++;
-
                 } else {
                     DAILY_STAKE--;
+
                 }
             }
+            if(DAILY_STAKE>100) {
+                totalAmountWonFor20Day+=50;
+                System.out.println("Won on day "+day + " is "+ (DAILY_STAKE-100));
+                daysWon++;
+            }
+            else {
+                totalAmountLostFor20Day-=50;
+                System.out.println("Lost on day "+day +" is "+ (100-DAILY_STAKE));
+                daysLost++;
+            }
+
         }
-        System.out.println("Total money left after 20 days is: " +DAILY_STAKE);
+
+        System.out.println("Total amount won in 20 days is "+ totalAmountWonFor20Day);
+        System.out.println("Total amount lost in 20 days is "+ totalAmountLostFor20Day);
+        System.out.println("No of days won is: "+ daysWon);
+        System.out.println("No of days Lost is: "+ daysLost);
 
     }
 }
