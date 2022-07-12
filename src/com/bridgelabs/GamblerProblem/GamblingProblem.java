bc3a7568aae0386b37dfe064c131f12d1f45e715
Abhishek Sagar <abhisheksagar1403@gmail.com>
2022-07-12 14:15:52 +0530

    Welcome message

---
+++ src/com/bridgelabs/GamblerProblem/GamblingProblem.java
@@ -0,0 +1,7 @@
+package com.bridgelabs.GamblerProblem;
+
+public class GamblingProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Simulator");
+    }
+}
